daa7bbe206fa27b6b88265568cdb6bb639426c24
mohan-1228 <103790326+mohan-1228@users.noreply.github.com>
2026-08-22 17:49:38 -0500

    Add guestbook and contact form backend

---
+++ amplify/data/resource.ts
@@ -0,0 +1,28 @@
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+
+const schema = a.schema({
+  GuestbookEntry: a
+    .model({
+      name: a.string().required(),
+      message: a.string().required(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+  ContactSubmission: a
+    .model({
+      name: a.string().required(),
+      email: a.string().required(),
+      message: a.string().required(),
+    })
+    .authorization((allow) => [allow.publicApiKey().to(['create'])]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 },
+  },
+});
